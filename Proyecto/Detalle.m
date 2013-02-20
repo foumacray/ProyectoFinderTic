b72cc111d85933f019a56ed7b3f6a3a0f70942f3
Alumno 6 <alumno1@iMac-de-Alumno-6.local>
2013-02-20 20:04:25 +0000

    mapa a falta de ajustarlo

--- Proyecto/Detalle.m
+++ Proyecto/Detalle.m
@@ -40,11 +40,10 @@
     _descripcion.text=[_arrayDetalle objectAtIndex:1];
     _inicio.text=[_arrayDetalle objectAtIndex:2];
     _fin.text=[_arrayDetalle objectAtIndex:3];
-    
-    
-    
-    CLLocationCoordinate2D pos ={28, -15};
-    MKCoordinateSpan zoom={10,10};
+    _longitud.text=[[self separarCoordenadas:[_arrayDetalle objectAtIndex:4]] objectAtIndex:0];
+    _latitud.text=[[self separarCoordenadas:[_arrayDetalle objectAtIndex:4]] objectAtIndex:1];
+    CLLocationCoordinate2D pos =[self coordenadasLatitud:_longitud.text coordenadasLongitud:_latitud.text];
+    MKCoordinateSpan zoom={0.1,0.1};
     
     MKCoordinateRegion region={pos,zoom};
     
@@ -55,6 +54,63 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(NSMutableArray*)separarCoordenadas:(NSString*)coordenadasJuntas{
+    // NSRange incidencia = [coordenadasJuntas rangeOfString:@" "];
+    NSMutableArray *miArray=[NSMutableArray new];
+
+    [miArray addObject:[coordenadasJuntas substringFromIndex:[coordenadasJuntas rangeOfString:@" "].location+1]];
+    [miArray addObject:[coordenadasJuntas substringToIndex:[coordenadasJuntas rangeOfString:@" "].location-1]];
+    return miArray;
+}
+- (CLLocationCoordinate2D)coordenadasLatitud:(NSString *)latitud coordenadasLongitud:(NSString *)longitud
+{
+    double cordLongitud = [longitud doubleValue];
+    double cordLatitud = [latitud doubleValue];
+    
+    double UTM=30;
+    double C=6371000;
+    double Ee=0.006739497;
+    double PI=3.14159265359;
+    float mericentral = (6*UTM-183); // Usamo el UTM =30, es la region cuadriculada de Zaragoza.
+    double surEcuador = cordLongitud;
+    float FI = (surEcuador/(6366197.724*0.9996)); // Bien
+    float var3 = pow(cosf(FI), 2);
+    float var2 = pow(1+Ee*var3, 0.500);
+    float NI = (C/var2)*0.9996; // Bien
+    float a = (cordLatitud - 500000)/NI; // Bien
+    float A1 = sinf(2*FI); // Bien
+    float A2 = A1*pow(cosf(FI), 2); // Bien
+    float J2 = FI+(A1/2); // Bien
+    float J4 = (3*J2+A2)/4; // Bien
+    float J6 = (5*J4+A2*(pow(cosf(FI), 2)))/3; // Bien
+    float Alfa = (Ee*3)/4; // Bien
+    float Beta =((5)*pow(Alfa, 2))/3; // Bien
+    float Gama = (35)*pow(Alfa, 3)/27; // Bien
+    double var6 = 0.9996*C;
+    double var7 = -(Alfa*J2)+(Beta*J6)-(Gama*A1);
+    double Bfi = var6*(var7+FI); //Mal
+    float b = (surEcuador-Bfi)/NI;
+    double var = pow(a, 2);
+    double var4 = (Ee*var)*0.500;
+    double var5 = var4*cosf(FI);
+    double Zeta = pow(var5, 2);
+    double Xi = a*(1-(Zeta/3)); // Bien
+    double Eta = b*(1-Zeta)+FI; // Bien
+    double SenhXi = (expf(Xi)-expf(-Xi))/2; // Bien
+    double DeltaLambda = atanf(SenhXi/cosf(Eta)); // Bien
+    double Tau = atanf(cosf(DeltaLambda)*tanf(Eta));
+    float var9 = pow(cosf(FI), 2);
+    float Frad = FI+(1+(Ee*(var9))-(1.500)*Ee*sinf(FI)*cosf(FI)*(Tau-FI))*(Tau-FI);
+    float LongitudFinalY = +(DeltaLambda/PI)*180+mericentral;
+    // Correción De valores, debido al excedente de decimales de float y double, afecta sobre todo al eje Y.
+     LongitudFinalY = LongitudFinalY - 0.001151;
+    NSLog(@"Cordenada Y = %f",LongitudFinalY);
+    float LatitudFinalX = +(Frad/PI)*180;
+    NSLog(@"Cordenada X = %f",LatitudFinalX);
+    
+    CLLocationCoordinate2D coordenadasFinal = {.latitude=LatitudFinalX, .longitude=LongitudFinalY};
+    return coordenadasFinal;
 }
 
 @end
